f60fc41d37631754fd501f428265c90dc4b46dc6
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2024-09-18 17:19:27 +0200

    chore: use com.gradleup.shadow for jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ val javaVersion = JvmTarget.JVM_21
 plugins {
     kotlin("jvm") version "2.0.20"
     id("com.diffplug.spotless") version "6.25.0"
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.1"
     id("com.github.ben-manes.versions") version "0.51.0"
     id("maven-publish")
     id("java-library")
